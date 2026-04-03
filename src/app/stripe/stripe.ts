260e201bdd2151dfd3342b06cbae0a32c327e67b
rafioul-hasan-58 <rafioulhasan2@.com>
2026-04-03 17:09:32 +0600

    fix: stripe construtor error fixed

--- src/app/stripe/stripe.ts
+++ src/app/stripe/stripe.ts
@@ -1,8 +1,6 @@
-import * as Stripe from "stripe";
+import Stripe from "stripe";
 import config from "../../config";
 
-const stripe = new (Stripe as any)(config.stripe.secret_key as string, {
-    apiVersion: "2025-12-15.clover",
-});
+const stripe = new Stripe(config.stripe.secret_key as string);
 
 export default stripe;
